b5a2032a9c52ed6ecb9943281e5387b349f6989b
Kim Kulling <kim.kulling@googlemail.com>
2019-07-09 22:17:54 +0200

    closes https://github.com/assimp/assimp/issues/1133: add enscripten check.

--- code/Common/DefaultIOStream.cpp
+++ code/Common/DefaultIOStream.cpp
@@ -129,7 +129,7 @@ size_t DefaultIOStream::FileSize() const
         if (0 != err)
             return 0;
         mCachedSize = (size_t) (fileStat.st_size);
-#elif defined __GNUC__ || defined __APPLE__ || defined __MACH__ || defined __FreeBSD__
+#elif defined __GNUC__ || defined __APPLE__ || defined __MACH__ || defined __FreeBSD__ || defined __EMSCRIPTEN__
         struct stat fileStat;
         int err = stat(mFilename.c_str(), &fileStat );
         if (0 != err)
